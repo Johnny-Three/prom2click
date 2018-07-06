42e12f62b57922a8efb733cba8da85d7558a25d1
xiejinke1 <xiejinke1@jd.com>
2018-07-06 14:35:03 +0800

    add label.go

---
+++ label/name.go
@@ -0,0 +1,13 @@
+package label
+
+
+const (
+	App = "app"
+	Ip = "ip"
+	Namespace = "namespace"
+    Job = "job"
+    Shard = "shard"
+    Keyspace = "keyspace"
+	Component = "component"
+	Container = "container_name"
+)
